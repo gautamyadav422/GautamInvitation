65986ce31eb1b5978f82ebf3549d81be53bf13ed
gautamyadav422 <64595557+gautamyadav422@users.noreply.github.com>
2021-10-19 20:54:06 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@ $(window).load(function(){
 $(document).ready(function(){
 
 //=====>  Countdown (Edit this with your own date)  <====
-$("#countdown").countdown("2021/11/26 23:59:59", function(event) {
+$("#countdown").countdown("2021/11/25 23:59:59", function(event) {
   var $this = $(this).html(event.strftime(''
      + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %-D </span> Days </div></div> '
      + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %H </span> Hours </div></div>'
